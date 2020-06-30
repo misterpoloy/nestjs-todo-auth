03a1613275519eeb31ae1461f5f211fd055a16f5
Juan Pablo Ortiz <jp@calaps.com>
2020-06-30 16:45:50 -0500

    user auth working

---
+++ src/auth/dto/auth-credentials.dto.ts
@@ -0,0 +1,15 @@
+import { isString } from "util";
+import { MinLength, MaxLength, IsString, Matches } from "class-validator";
+
+export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  username: string;
+
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, { message: "Passwrod too weak"})
+  password: string;
+}
---
+++ src/auth/user.repository.ts
@@ -0,0 +1,32 @@
+import { Repository, EntityRepository } from "typeorm";
+import * as bcrypt from "bcrypt";
+import { User } from "./user.entity";
+import { AuthCredentialsDto } from "./dto/auth-credentials.dto";
+import { ConflictException, InternalServerErrorException } from "@nestjs/common";
+
+@EntityRepository(User)
+export class UserRepository extends Repository<User> {
+
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+    const user = new User();
+    user.username = username;
+    user.salt = await bcrypt.genSalt();
+    user.password = await this.hashPassword(password, user.salt);
+
+    try {
+      await user.save();
+    } catch(error) {
+      if (error.code === '23505') { // Duplicate username
+        throw new ConflictException('User already exist');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
+  }
+
+  private async hashPassword(password: string, salt: string): Promise<string> {
+    console.log(password, salt);
+    return bcrypt.hash(password, salt);
+  }
+}
